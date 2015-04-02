c5439576c96069e2238a6c9ad325e69f0362e5d3
Graham Ollis <plicease@cpan.org>
2015-04-02 08:09:04 -0400

    standard layout

--- clean.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-if "%MSVCDir%"=="" call msvs6env
-if "%MSSdk%"=="" call psdk6env
-
-if exist Makefile call nmake realclean
-if exist package\MSWin32-x86-multi-thread rd /q/s package\MSWin32-x86-multi-thread
